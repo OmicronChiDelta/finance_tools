119a4f562b195c6a17042f001e91dd80e4cc5d61
Alex <50920163+OmicronChiDelta@users.noreply.github.com>
2022-01-25 19:50:19 +0000

    all basic functionality working, code could do with a tidy

--- mortgage_app.py
+++ mortgage_app.py
@@ -7,8 +7,8 @@ if "E:\\finance_tools" not in sys.path:
     sys.path.append("E:\\finance_tools")
 from utils import calc_min_payment, calc_balance
 
-from bokeh.models import Slider, ColumnDataSource
-from bokeh.layouts import column
+from bokeh.models import Slider, ColumnDataSource, MultiLine, Plot
+from bokeh.layouts import column, row
 from bokeh.palettes import RdYlBu3
 from bokeh.plotting import figure, curdoc
 
@@ -26,44 +26,51 @@ slide_r    = Slider(start=1,      end=7,        value=1.04,   step=0.01, title="
 slide_term = Slider(start=10,     end=max_term, value=30,     step=1,    title="Mortgage term")
 slide_fix  = Slider(start=0,      end=10,       value=5,      step=1,    title="Fixed-term length")
 slide_p    = Slider(start=100000, end=max_p,    value=208499, step=1,    title="Principal")
+slide_over = Slider(start=0,     end=500,      value=0,      step=1,    title="Fixed monthly overpayment")
 
 #Compute graphic from defaults
 m = calc_min_payment(slide_p.value, slide_r.value, slide_term.value)
 month_ids = [i for i in range(12*slide_term.value)]
 frac_years = [i/12 for i in month_ids]
-t_min = [calc_balance(slide_p.value, slide_r.value, j, m) for j in month_ids]
+t_min = [calc_balance(slide_p.value, slide_r.value, j, m+slide_over.value) for j in month_ids]
+c = ["red" if i/12 < slide_fix.value else "lightsalmon" for i in range(len(frac_years) -1)]
 
 #Insert data into CDS
-source = ColumnDataSource(data={'x':frac_years, 'y':t_min}) 
-
+source = ColumnDataSource(data={'xs':[[frac_years[i], frac_years[i+1]] for i, _ in enumerate(frac_years) if i < len(frac_years)-1], 
+                                'ys':[[t_min[i], t_min[i+1]] for i, _ in enumerate(t_min) if i < len(t_min)-1], 
+                                'line_color':c}) 
+ 
 #Configure plot
-trajectory = p.line(x='x', y='y', line_width=2, source=source)
+trajectory = p.multi_line(xs="xs", ys="ys", line_color="line_color", line_width=5, source=source)
+
 ds = trajectory.data_source
-
-
 
 #%%Callback to recalculate repayment trajectory
 def callback(attr, old, new):
         
-    m = calc_min_payment(208499, new, 30)
+    m = calc_min_payment(slide_p.value, slide_r.value, slide_term.value)
     
-    month_ids = [i for i in range(12*30)]
+    month_ids = [i for i in range(12*slide_term.value)]
     frac_years = [i/12 for i in month_ids]
     
     #Minimal payment
-    t_min = [calc_balance(208499, new, j, m) for j in month_ids]
+    t_min = [calc_balance(slide_p.value, slide_r.value, j, m+slide_over.value) for j in month_ids]
+    c = ["red" if i/12 < slide_fix.value else "lightsalmon" for i in range(len(frac_years) -1)]
+
     
     #Populate dictionary behind CDS with new data
-    new_data = {'x':frac_years, 'y':t_min}
+    new_data = {'xs':[[frac_years[i], frac_years[i+1]] for i, _ in enumerate(frac_years) if i < len(frac_years)-1],
+	            'ys':[[t_min[i], t_min[i+1]] for i, _ in enumerate(t_min) if i < len(t_min)-1],
+	            "line_color":c}
+
     ds.data = new_data
-
-
 
 #%% Configure final plot
 slide_r.on_change('value', callback)
-#slide_term.on_change('value', callback)
-#slide_fix.on_change('value', callback)
-#slide_p.on_change('value', callback)
+slide_term.on_change('value', callback)
+slide_fix.on_change('value', callback)
+slide_p.on_change('value', callback)
+slide_over.on_change('value', callback)
 
 # put sliders and plot in a layout and add to the document
-curdoc().add_root(column(p, slide_r, slide_term, slide_fix, slide_p))
+curdoc().add_root(row(p, column(slide_r, slide_term, slide_fix, slide_p, slide_over)))
