69df28ea2eaf4ec0d2ab42e21330929fc3cada04
Alex <50920163+OmicronChiDelta@users.noreply.github.com>
2022-02-14 12:28:07 +0000

    tidying visuals and introducing monthly repayment and overpayment visual

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from bokeh.models import Slider, ColumnDataSource, MultiLine, Plot
 from bokeh.layouts import column, row
 from bokeh.plotting import figure, curdoc
+from bokeh.models import NumeralTickFormatter
 
 #PREP FOR DOCKER FILE - use path manager instead
 # import sys
@@ -24,7 +25,14 @@ primary_b = "dodgerblue"
 
 
 #%% Graphics
-p = figure(x_range=(0, max_term), y_range=(0, max_p), toolbar_location=None)
+p = figure(x_range=(0, max_term), y_range=(0, max_p), toolbar_location=None, x_axis_label='Time / years', y_axis_label='Balance / £')
+p.yaxis.formatter=NumeralTickFormatter(format="00")
+p.yaxis.ticker.desired_num_ticks = 40
+p.yaxis.minor_tick_line_color = None  #turn off y-axis minor ticks
+
+q = figure(x_range=["A", "B"], y_range=(0, 2000), toolbar_location=None, plot_width=300, plot_height=350, x_axis_label='Deal', y_axis_label='Monthly repayment / £')
+q.yaxis.ticker.desired_num_ticks = 20
+q.yaxis.minor_tick_line_color = None  #turn off y-axis minor ticks
 
 slide_r_a    = Slider(start=1,      end=7,        value=1.04,   step=0.01, title="Interest rate")
 slide_term_a = Slider(start=10,     end=max_term, value=30,     step=1,    title="Mortgage term")
@@ -39,21 +47,44 @@ slide_p_b    = Slider(start=100000, end=max_p,    value=208499, step=1,    title
 slide_over_b = Slider(start=0,      end=1000,     value=300,    step=1,    title="Fixed monthly overpayment")
 
 #Insert data into CDS
-source_a = ColumnDataSource(data=make_trajectory(slide_p_a, slide_r_a, slide_term_a, slide_over_a, slide_fix_a, primary_a))
-source_b = ColumnDataSource(data=make_trajectory(slide_p_b, slide_r_b, slide_term_b, slide_over_b, slide_fix_b, primary_b))
- 
-#Configure plot
+source_a = ColumnDataSource(data=make_trajectory(slide_p_a, slide_r_a, slide_term_a, slide_over_a, slide_fix_a, primary_a)[0])
+source_b = ColumnDataSource(data=make_trajectory(slide_p_b, slide_r_b, slide_term_b, slide_over_b, slide_fix_b, primary_b)[0])
+source_ra = ColumnDataSource(data=make_trajectory(slide_p_a, slide_r_a, slide_term_a, slide_over_a, slide_fix_a, primary_a)[1])
+source_ra.data.update({"xs":["A"]})
+source_rb = ColumnDataSource(data=make_trajectory(slide_p_b, slide_r_b, slide_term_b, slide_over_b, slide_fix_b, primary_b)[1])
+source_rb.data.update({"xs":["B"]})
+
+#Configure plots
 trajectory_a = p.multi_line(xs="xs", ys="ys", line_color="line_color", line_width=5, source=source_a)
 trajectory_b = p.multi_line(xs="xs", ys="ys", line_color="line_color", line_width=5, source=source_b)
+rep_a_min = q.vbar(x="xs", top="ymin_top", bottom="ymin_bot", width=0.5, source=source_ra, fill_color=primary_a)
+rep_a_over = q.vbar(x="xs", top="yover_top", bottom="yover_bot", width=0.5, source=source_ra, fill_color=primary_a, hatch_pattern="@")
+rep_b_min = q.vbar(x="xs", top="ymin_top", bottom="ymin_bot", width=0.5, source=source_rb, fill_color=primary_b)
+rep_b_over = q.vbar(x="xs", top="yover_top", bottom="yover_bot", width=0.5, source=source_rb, fill_color=primary_b, hatch_pattern="@")
 
 
 
 #%% Configure functionality and positioning
 def callback_a(attr, old, new):
-    trajectory_a.data_source.data = make_trajectory(slide_p_a, slide_r_a, slide_term_a, slide_over_a, slide_fix_a, primary_a)
-	
+    traj_update = make_trajectory(slide_p_a, slide_r_a, slide_term_a, slide_over_a, slide_fix_a, primary_a)
+    trajectory_a.data_source.data = traj_update[0]
+    
+    rep_a_min.data_source.data = traj_update[1]
+    rep_a_over.data_source.data = traj_update[1]
+    
+    rep_a_min.data_source.data.update({"xs":["A"]})
+    rep_a_over.data_source.data.update({"xs":["A"]})
+
+    
 def callback_b(attr, old, new):
-	trajectory_b.data_source.data = make_trajectory(slide_p_b, slide_r_b, slide_term_b, slide_over_b, slide_fix_b, primary_b)
+    traj_update = make_trajectory(slide_p_b, slide_r_b, slide_term_b, slide_over_b, slide_fix_b, primary_b)
+    trajectory_b.data_source.data = traj_update[0]
+
+    rep_b_min.data_source.data = traj_update[1]
+    rep_b_over.data_source.data = traj_update[1]
+    
+    rep_b_min.data_source.data.update({"xs":["B"]})
+    rep_b_over.data_source.data.update({"xs":["B"]})
 
 slide_r_a.on_change('value',    callback_a)
 slide_term_a.on_change('value', callback_a)
@@ -68,4 +99,4 @@ slide_p_b.on_change('value',    callback_b)
 slide_over_b.on_change('value', callback_b)
 
 # put sliders and plot in a layout and add to the document
-curdoc().add_root(row(p, column(slide_r_a, slide_term_a, slide_fix_a, slide_p_a, slide_over_a), column(slide_r_b, slide_term_b, slide_fix_b, slide_p_b, slide_over_b)))
+curdoc().add_root(row(p, column(slide_r_a, slide_term_a, slide_fix_a, slide_p_a, slide_over_a, q), column(slide_r_b, slide_term_b, slide_fix_b, slide_p_b, slide_over_b)))
--- utils_graphics.py
+++ utils_graphics.py
@@ -16,29 +16,31 @@ from matplotlib.colors import rgb2hex, to_rgba
 
 def make_trajectory(p_slider, r_slider, term_slider, over_slider, fix_slider, traj_colour):
 
-	"""
-	Create/update data structure powering each repayment visual using current state from each slider
-	"""
-
-	#Timebase
-	month_ids = [i for i in range(12*term_slider.value)]
-	frac_years = [i/12 for i in month_ids]
-	
-	repay_min = calc_min_payment(p_slider.value, r_slider.value, term_slider.value)
-	
-	#Trajectory @ min. monthly repayment
-	traj_min = [calc_balance(p_slider.value, r_slider.value, j, repay_min+over_slider.value) for j in month_ids]
-	
-	#Indicate where fixed term ends
-	fade = rgb2hex(to_rgba(traj_colour, alpha=0.1), keep_alpha=True)
-	c = [traj_colour if i/12 < fix_slider.value else fade for i in range(len(frac_years) -1)]
-	
-    #"list of lists" format
-	data_dict = {'xs':[[frac_years[i], frac_years[i+1]] for i, _ in enumerate(frac_years) if i < len(frac_years)-1],
-	             'ys':[[traj_min[i], traj_min[i+1]] for i, _ in enumerate(traj_min) if i < len(traj_min)-1],
-	             "line_color":c}
-	
-	return data_dict
+    """
+    Create/update data structure powering each repayment visual using current state from each slider
+    """
+    
+    #Timebase
+    month_ids = [i for i in range(12*term_slider.value)]
+    frac_years = [i/12 for i in month_ids]
+    
+    repay_min = calc_min_payment(p_slider.value, r_slider.value, term_slider.value)
+    
+    #Trajectory @ min. monthly repayment
+    traj_min = [calc_balance(p_slider.value, r_slider.value, j, repay_min+over_slider.value) for j in month_ids]
+    
+    #Indicate where fixed term ends
+    fade = rgb2hex(to_rgba(traj_colour, alpha=0.1), keep_alpha=True)
+    c = [traj_colour if i/12 < fix_slider.value else fade for i in range(len(frac_years) -1)]
+    
+       #"list of lists" format
+    data_dict = {'xs':[[frac_years[i], frac_years[i+1]] for i, _ in enumerate(frac_years) if i < len(frac_years)-1],
+                 'ys':[[traj_min[i], traj_min[i+1]] for i, _ in enumerate(traj_min) if i < len(traj_min)-1],
+                 "line_color":c}
+    
+    repay_dict = {'ymin_bot':[0], 'ymin_top':[repay_min], 'yover_bot':[repay_min], 'yover_top':[repay_min + over_slider.value]}
+       
+    return data_dict, repay_dict
 
 
 
